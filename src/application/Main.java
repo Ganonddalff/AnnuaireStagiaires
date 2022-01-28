cc54dd632aac59ffb43f600b8b728b47313eeae0
Ganonddalff <m-ateusz@live.fr>
2022-01-28 16:13:51 +0100

    Ajout dans le main du lecteur du fichier.
    Le lecteur BufferedReader lit le .DON dans la console ligne par ligne.
    
    Signed-off-by: Ganonddalff <m-ateusz@live.fr>

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,16 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import java.io.*;
 
 
 public class Main extends Application {
 	@Override
+
+
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
@@ -19,11 +22,28 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
+
 	public static void main(String[] args) {
 		launch(args);
-	}
-	
-	
-}
 
+		String file = "/home/matt/Documents/ISIKA/Projet1/STAGIAIRES.DON"; // On met le fichier et son adresse dans le String file
+
+
+		//On invoque BufferedReader pour lire les gros fichiers texte
+		try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+			//br va lire ligne par ligne on crée donc une variable line
+			String line;
+			while((line = br.readLine()) != null) { //tant que line n'est pas nul on continue
+				System.out.println(line);
+			}
+
+		}
+		catch(IOException e) {
+			System.out.println("Une erreur est survenue. Déso.");
+			e.printStackTrace();
+		}
+
+	}
+}
