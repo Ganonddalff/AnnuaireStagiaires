c46a076b8dd38e2502b50bdddf66ea5be5347590
yannsen <ysenic@gmail.com>
2022-01-28 14:43:44 +0100

    ajout bis classe Noeud Stagiaire Personne

---
+++ src/application/Noeud.java
@@ -0,0 +1,50 @@
+package application;
+
+public class Noeud {
+	// déclaration des attributs
+	Stagiaire stagiaire =new Stagiaire();
+	Noeud noeudDroit = new Noeud();
+	Noeud noeudGauche = new Noeud();
+	
+	// déclaration des constructeurs
+	public Noeud() {
+	}
+
+	public Noeud(Stagiaire stagiaire,Noeud noeudDroit,Noeud noeudGauche) {
+		this.stagiaire=stagiaire;
+		this.noeudDroit=noeudDroit;
+		this.noeudGauche=noeudGauche;
+		
+	}
+	//  getters et setters
+
+	public Stagiaire getStagiaire() {
+		return stagiaire;
+	}
+
+	public void setStagiaire(Stagiaire stagiaire) {
+		this.stagiaire = stagiaire;
+	}
+
+	public Noeud getNoeudDroit() {
+		return noeudDroit;
+	}
+
+	public void setNoeudDroit(Noeud noeudDroit) {
+		this.noeudDroit = noeudDroit;
+	}
+
+	public Noeud getNoeudGauche() {
+		return noeudGauche;
+	}
+
+	public void setNoeudGauche(Noeud noeudGauche) {
+		this.noeudGauche = noeudGauche;
+	}
+	
+	
+	
+	
+	
+	
+}
