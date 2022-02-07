12f2bb0e3c8932a69fbc7f59dfdf6d13dc832172
Ganonddalff <m-ateusz@live.fr>
2022-02-07 14:17:55 +0100

    Amélioration interface graphique
    Signed-off-by: Ganonddalff <m-ateusz@live.fr>

--- src/application/Main.java
+++ src/application/Main.java
@@ -35,8 +35,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
@@ -53,10 +51,10 @@ public class Main extends Application {
 
 		try {
 			//création de root
-			BorderPane root = new BorderPane();
+			GridPane root = new GridPane();
 
-			//création d'une scère 1600x900
-			final Scene scene = new Scene(root,1600,900,Color.DIMGRAY);
+			//création d'une scène 
+			Scene scene = new Scene(root,850,900,Color.DIMGRAY);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
 
@@ -67,41 +65,41 @@ public class Main extends Application {
 			// Choix de la couleur de fond de l'app
 			scene.setFill(Color.GRAY);
 			primaryStage.setScene(scene);
-			//Création de myMenu qui est une barre de menu
-			MenuBar myMenu = new MenuBar();
-
-			//Création des menus
-			Menu menuFichier = new Menu("Fichier");
-			Menu menuEditer = new Menu("Editer");
-			Menu menuAide = new Menu("Aide");
-
-
-			//Création des items du menu pour Fichier :
-			MenuItem nouveauItem = new MenuItem("Nouveau");
-			MenuItem importItem = new MenuItem("Importer un fichier texte");
-			MenuItem recherItem = new MenuItem("Rechercher");
-			MenuItem recherAvItem = new MenuItem("Rechercher...");
-			MenuItem quitterItem = new MenuItem("Quitter");
-
-			//Création des items du menu Editer : 
-			MenuItem copierItem = new MenuItem ("Copier");
-			MenuItem collerItem = new MenuItem("Coller");
-			MenuItem ajouterItem = new MenuItem ("Ajouter");
-			MenuItem modifItem = new MenuItem ("Modifier");
-			MenuItem supprItem = new MenuItem ("Supprimer");
-
-			//Création de l'item d'aide
-			MenuItem aideItem = new MenuItem("Aide");
-
-			//Ajouter les items aux menus
-			menuFichier.getItems().addAll(nouveauItem,importItem,recherItem,recherAvItem,quitterItem);
-			menuEditer.getItems().addAll(copierItem,collerItem,ajouterItem,modifItem,supprItem);
-			menuAide.getItems().add(aideItem);
-
-			//ajouter les menus à la barre : 
-			myMenu.getMenus().addAll(menuFichier,menuEditer,menuAide); 
-			//afficher la barre de menu
-			root.setTop(myMenu);
+			//			//Création de myMenu qui est une barre de menu
+			//			MenuBar myMenu = new MenuBar();
+			//
+			//			//Création des menus
+			//			Menu menuFichier = new Menu("Fichier");
+			//			Menu menuEditer = new Menu("Editer");
+			//			Menu menuAide = new Menu("Aide");
+			//
+			//
+			//			//Création des items du menu pour Fichier :
+			//			MenuItem nouveauItem = new MenuItem("Nouveau");
+			//			MenuItem importItem = new MenuItem("Importer un fichier texte");
+			//			MenuItem recherItem = new MenuItem("Rechercher");
+			//			MenuItem recherAvItem = new MenuItem("Rechercher...");
+			//			MenuItem quitterItem = new MenuItem("Quitter");
+			//
+			//			//Création des items du menu Editer : 
+			//			MenuItem copierItem = new MenuItem ("Copier");
+			//			MenuItem collerItem = new MenuItem("Coller");
+			//			MenuItem ajouterItem = new MenuItem ("Ajouter");
+			//			MenuItem modifItem = new MenuItem ("Modifier");
+			//			MenuItem supprItem = new MenuItem ("Supprimer");
+			//
+			//			//Création de l'item d'aide
+			//			MenuItem aideItem = new MenuItem("Aide");
+			//
+			//			//Ajouter les items aux menus
+			//			menuFichier.getItems().addAll(nouveauItem,importItem,recherItem,recherAvItem,quitterItem);
+			//			menuEditer.getItems().addAll(copierItem,collerItem,ajouterItem,modifItem,supprItem);
+			//			menuAide.getItems().add(aideItem);
+			//
+			//			//ajouter les menus à la barre : 
+			//			myMenu.getMenus().addAll(menuFichier,menuEditer,menuAide); 
+			//			//afficher la barre de menu
+			//			//	root.setTop(myMenu);
 
 			//Couleur root transparante pour couleur Pane seulement
 			root.setStyle("-fx-background-color: null;");
@@ -110,117 +108,110 @@ public class Main extends Application {
 
 			//Création du bouton ajouter
 			Button ajouterBtn = new Button("Ajouter un stagiaire");
-			ajouterBtn.setLayoutX(700);
-			ajouterBtn.setLayoutY(450);
-
-
-			//création du bouton afficher
-			Button afficherBtn = new Button("Afficher la liste");
-			afficherBtn.setLayoutX(300);
-			afficherBtn.setLayoutY(450);
+			ajouterBtn.setPrefSize(150, 20);
 
 
 			//Création du bouton modifier un élément de la liste
 			Button modifierBtn = new Button("Modifier un stagiaire");
-			modifierBtn.setLayoutX(1100);
-			modifierBtn.setLayoutY(450);
+			modifierBtn.setPrefSize(150, 20);
+			
+			//Création du bouton exportPDF
+			Button exportPDF = new Button("Exporter en pdf");
+			exportPDF.setPrefSize(150, 20);
+			
 
-			//grouper les boutons
-			Group groupeStagiaire = new Group();
-			groupeStagiaire.getChildren().addAll(ajouterBtn,afficherBtn,modifierBtn);
+			//Le titre de la liste situé en haut
+			Text titreListe = new Text("Liste des stagiaires");
+			titreListe.setFont(Font.font("Tahoma", FontWeight.THIN, 20));
+			
+			//Un champ de recherche
+			Label rechercheLabel = new Label(" Recherche ");
+			TextField rechecherChamp = new TextField();
+			
+			// un bouton rechercher
+			Button rechercherBtn = new Button("Lancer la recherche");
+			rechercherBtn.setPrefSize(150, 20);
 
-			//ajouter le groupe à root
+			
+			
 
-			root.getChildren().add(groupeStagiaire);
 
-			//Pas de couleur pour menubar
-			myMenu.setStyle("-fx-background-color: null;");
+			/*** LES CHOSES SERIEUSES : AFFICHER
+			 * CETTE FICHUE 
+			 * LISTE
+			 */
+			//Création de l'objet tableStagiaire
+			TableView<Stagiaire> tableStagiaire = new TableView<Stagiaire>();
+
+			//Création des colonnes du tableau
+			//Colonne Nom
+			TableColumn<Stagiaire, String> colonneNom = new TableColumn<Stagiaire, String>("Nom") ;
+			//Colonne Prénom
+			TableColumn<Stagiaire, String> colonnePrenom = new TableColumn<Stagiaire, String>("Prénom");
+			//Colonne Département
+			TableColumn<Stagiaire, String> colonneDept = new TableColumn<Stagiaire, String>("Département");
+			//Colonne Promo
+			TableColumn<Stagiaire, String> colonnePromo = new TableColumn<Stagiaire, String>("Promotion");
+			//Colonne Année 
+			TableColumn<Stagiaire, String> colonneAnnee = new TableColumn<Stagiaire, String>("Année");
+
+			//Ajouter les colonnes à la table
+
+			tableStagiaire.getColumns().addAll(colonneNom,colonnePrenom,colonneDept,colonnePromo,colonneAnnee);
+
+			//Donner leurs valeurs aux colonnes 
+			colonneNom.setCellValueFactory(new PropertyValueFactory<>("nom"));
+			colonnePrenom.setCellValueFactory(new PropertyValueFactory<>("prenom"));
+			colonneDept.setCellValueFactory(new PropertyValueFactory<>("codeDepartement"));
+			colonnePromo.setCellValueFactory(new PropertyValueFactory<>("promo"));
+			colonneAnnee.setCellValueFactory(new PropertyValueFactory<>("dateEntree"));
+
+
+			//Trier par ordre alphabétiqueDépartement
+			//		colonneNom.setSortType(SortType.ASCENDING);
+			//			colonnePrenom.setSortable(false);
+
+			ObservableList<Stagiaire> listeStagiaires = FXCollections.observableArrayList();
+
+
+			FichierATraiter.ecrireStagiaireFBDsListObs(0,listeStagiaires);
+
+			tableStagiaire.setItems(listeStagiaires);
+			System.out.println(listeStagiaires);
+
+			tableStagiaire.setPrefHeight(800);
+			tableStagiaire.setPrefWidth(582);
+
+
+
+			
+			//VBox pour les boutons sur la droite
+			VBox boutons = new VBox();
+			
+			boutons.getChildren().addAll(ajouterBtn,modifierBtn,exportPDF);
+			boutons.setSpacing(50);
+			boutons.setAlignment(Pos.CENTER_RIGHT);
+		//	boutons.setPadding(new Insets(15,0,40,10));
+			
+			
+			
+			//Positionnement
+			
+			root.add(titreListe,1,0);
+			root.add(rechercheLabel, 0, 1);
+			root.add(rechecherChamp, 1, 1);
+			root.add(rechercherBtn, 2, 1);
+			root.add(tableStagiaire,1,2);
+			root.add(boutons, 2, 2);
+
+
+			//Espacement entre les items
+			root.setVgap(10);
+			root.setHgap(10);
+
 
 			//Lancement de primaryStage
 			primaryStage.show();
-
-			//Action appuyer sur afficher pour afficher la liste
-			afficherBtn.setOnAction(new EventHandler<ActionEvent>() {
-
-				@Override
-				public void handle(ActionEvent arg0) {
-					// TODO Auto-generated method stub
-
-					//On créer un stage, un GridPane et une Scene
-					//Sur lesquels on viendra poser la TableView
-					Stage afficherStage = new Stage();
-					StackPane stackAfficher= new StackPane();
-					stackAfficher.setAlignment(Pos.TOP_CENTER);
-					//					stackAfficher.setHgap(10);
-					//					stackAfficher.setVgap(10);
-					stackAfficher.setPadding(new Insets(25, 25, 25, 25));
-					Scene sceneAfficher = new Scene(stackAfficher,900,900);
-					scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-					afficherStage.setScene(sceneAfficher);
-					afficherStage.setTitle("Liste des stagiaires");
-					afficherStage.show();
-
-
-					//Le titre de la liste situé en haut
-					Text titreListe = new Text("Liste des stagiaires");
-					titreListe.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-					//					stackAfficher.add(titreListe, 0, 0);
-
-					/*** LES CHOSES SERIEUSES : AFFICHER
-					 * CETTE FICHUE 
-					 * LISTE
-					 */
-					//Création de l'objet tableStagiaire
-					TableView<Stagiaire> tableStagiaire = new TableView<Stagiaire>();
-
-					//Création des colonnes du tableau
-					//Colonne Nom
-					TableColumn<Stagiaire, String> colonneNom = new TableColumn<Stagiaire, String>("Nom") ;
-					//Colonne Prénom
-					TableColumn<Stagiaire, String> colonnePrenom = new TableColumn<Stagiaire, String>("Prénom");
-					//Colonne Département
-					TableColumn<Stagiaire, String> colonneDept = new TableColumn<Stagiaire, String>("Département");
-					//Colonne Promo
-					TableColumn<Stagiaire, String> colonnePromo = new TableColumn<Stagiaire, String>("Promotion");
-					//Colonne Année 
-					TableColumn<Stagiaire, String> colonneAnnee = new TableColumn<Stagiaire, String>("Année");
-
-					//Ajouter les colonnes à la table
-
-					tableStagiaire.getColumns().addAll(colonneNom,colonnePrenom,colonneDept,colonnePromo,colonneAnnee);
-
-					//Donner leurs valeurs aux colonnes 
-					colonneNom.setCellValueFactory(new PropertyValueFactory<>("nom"));
-					colonnePrenom.setCellValueFactory(new PropertyValueFactory<>("prenom"));
-					colonneDept.setCellValueFactory(new PropertyValueFactory<>("codeDepartement"));
-					colonnePromo.setCellValueFactory(new PropertyValueFactory<>("promo"));
-					colonneAnnee.setCellValueFactory(new PropertyValueFactory<>("dateEntree"));
-
-
-					//Trier par ordre alphabétiqueDépartement
-					colonneNom.setSortType(SortType.ASCENDING);
-					colonnePrenom.setSortable(false);
-
-					ObservableList<Stagiaire> listeStagiaires = FXCollections.observableArrayList();
-
-
-					FichierATraiter.ecrireStagiaireFBDsListObs(0,listeStagiaires);
-
-					tableStagiaire.setItems(listeStagiaires);
-					System.out.println(listeStagiaires);
-
-
-
-
-
-
-					//Ajouter la dite table à l'écran : 
-
-					stackAfficher.getChildren().add(tableStagiaire);
-
-				}
-			});
-
 
 			//Action appuyer sur ajouter ouvre une nouvelle fenêtre
 
@@ -357,11 +348,14 @@ public class Main extends Application {
 								//On crée le stagiaire
 								Stagiaire newStagiaire = new Stagiaire(nomStagiaire,prenomStagiaire,deptStagiaire,promoStagiaire,dateEntreeStagiaire);
 
-								System.out.println(newStagiaire.getNom()+"\n"+
-										newStagiaire.getPrenom()+"\n"+
-										newStagiaire.getCodeDepartement()+"\n"+
-										newStagiaire.getPromo()+"\n"+
-										newStagiaire.getDateEntree());
+								//								System.out.println(newStagiaire.getNom()+"\n"+
+								//										newStagiaire.getPrenom()+"\n"+
+								//										newStagiaire.getCodeDepartement()+"\n"+
+								//										newStagiaire.getPromo()+"\n"+
+								//										newStagiaire.getDateEntree());
+								int index=0;
+								index=FichierATraiter.ecrire1BlocDsFichierBinaire(newStagiaire);	
+								FichierATraiter.rechercheParentDsAB(0,newStagiaire,index);
 
 								Label succesAjout = new Label("Créé avec succès.\nVous pouvez fermer cette fenêtre.");
 								gridAjout.add(succesAjout, 3, 7);
@@ -464,6 +458,12 @@ public class Main extends Application {
 									TextField dept = new TextField(listestagiaires.get(i).getCodeDepartement());		
 									TextField promo = new TextField(listestagiaires.get(i).getPromo());
 									TextField annee = new TextField(""+listestagiaires.get(i).getDateEntree());
+									//Récupérer Stagiaire à supprimer
+									//									String;
+									//									String;
+									//									String;
+									//									String;
+									//									int;
 
 									//Positonnement des éléments à afficher
 									gridModif.add(nomLabel,0,3);
@@ -499,9 +499,9 @@ public class Main extends Application {
 											for(int i=0; i<listestagiaires.size();i++ ) {
 
 												if(listestagiaires.get(i).getNom().startsWith(champNom.getText().toUpperCase())) {
-													
-													FichierATraiter.majStagiaire(new Stagiaire(nom.getText(),prenom.getText(),dept.getText(),promo.getText(),Integer.parseInt(annee.getText())), i);
-													
+
+													//	FichierATraiter.majStagiaire(new Stagiaire(nom.getText(),prenom.getText(),dept.getText(),promo.getText(),Integer.parseInt(annee.getText())), i);
+
 													System.out.println();
 												}}
 
