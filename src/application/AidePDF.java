6d0d4a211ded976020284260f17f608b95205067
Ganonddalff <m-ateusz@live.fr>
2022-02-08 17:11:16 +0100

    Merge de la branche "yann"
    Version 1.0.0
    
    Signed-off-by: Ganonddalff <m-ateusz@live.fr>

--- src/application/AidePDF.java
+++ src/application/AidePDF.java
@@ -13,7 +13,7 @@ public class AidePDF {
 		  public static String  monPDFgo (){
 		    try 
 		    {
-		    	File f  = new File("/Users/antoine/Documents/Me/Informatique/ISIKA/projet_1_fichiers/liste.pdf");
+		    	File f  = new File("/home/matt/Documents/ISIKA/Projet1/liste.pdf");
 		    	Desktop.getDesktop().open(f);
 		    } catch (Exception ex) {
 		        ex.printStackTrace();
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,9 @@
 module template {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.base;
+	requires java.desktop;
+	requires kernel;
+	requires itextpdf;
 	opens application to javafx.graphics, javafx.fxml,javafx.base;
 }
